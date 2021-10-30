41b665ca258c1414865b6aa6ef78486aae305f6c
Onesimus Sarpong Wiafe <sarpnissi4@gmail.com>
2021-10-30 14:34:58 +0000

    Set up database connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,19 @@
-const auth = 3;
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const uri = process.env.MONGO_URI;
+
+const connectDB = () => {
+  return mongoose.connect(
+    uri,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    //   useCreateIndex: true,
+    //   useFindAndModify: false,
+    },
+    () => console.log(`Database connected successfully`)
+  );
+};
+
+module.exports = connectDB;
