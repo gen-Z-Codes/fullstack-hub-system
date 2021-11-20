c394299d644d23091ac9f7d018c67ae553160928
Ebenezer Abbey Anertey <69726819+ebenezer-std@users.noreply.github.com>
2021-11-20 23:03:15 +0000

    Created Email Schema

--- server/models/Email.js
+++ server/models/Email.js
@@ -1,1 +1,27 @@
-const email 
+const {Schema, model} = require('mongoose');
+
+const emailSchema = new Schema(
+    {
+        recipient:{
+            type: Types.ObjectId,
+            ref: "Customer",
+            required: [true,"Please provide the recipient details"]
+        },
+
+        header:{
+            type: String,
+            required: true
+        },
+
+        body:{
+            type: String,
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Email = model("Email",emailSchema);
+
