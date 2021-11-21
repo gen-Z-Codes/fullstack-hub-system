629546455645cf3b8552c6646ac1911ad23e6481
Ebenezer Abbey Anertey <69726819+ebenezer-std@users.noreply.github.com>
2021-11-21 23:00:52 +0000

    Updated Directory Structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/login.component";
+import SignUp from "./components/register.component";
+import DashBoard from "./Pages-working/DashBoard";
+import Customer from "./Pages-working/Customer";
+import Profile from "./Pages-working/Profile";
+import AddContact from "./Pages-working/AddContact";
 
 function App() {
-  return (
+  return (<Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      
+
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path="/sign-in" component={Login} />
+            <Route path="/sign-up" component={SignUp} />
+            <Route path="/dashboard" component={DashBoard} />
+            <Route path="/customer" component={Customer} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/addContact" component={AddContact} />
+          </Switch>
+        </div>
+      </div>
+    </div></Router>
   );
 }
 
